c6cdced40d9977810f863dde2312968bad62c00e
omxchavan <chavanom775@gmail.com>
2025-08-05 19:20:09 +0530

    same node server in express without file saving

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
 const express = require('express');
-//basic server 
 
-const myServer = http.createServer((req,res) =>{
-    console.log("welcome to my server");
+ 
+const app =express();
 
-// log out put for data.txt
-    const log=`${Date.now()} : ${req.method}\n`;
-
-//using fs for file handling
-    fs.appendFile("data.txt",log,(err,data) => {
-        res.end("data recieved")
-
-    });
- // parsind the url to get proper formatted data
-    const myurl=url.parse(req.url)
-    console.log(myurl)
-
-//learning url basics
-   
-    switch(req.url){
-        case "/" :
-            res.end("this is home page")
-        break;
-        case "/about":
-            res.end("this is about page ")
-        break;
-    }
-   
-    
+app.get("/",(req,res) =>{
+    res.send("we are at home page")
 })
 
-myServer.listen(3000,()=>{
-console.log("server started")
-}
-)
+app.get("/about",(req,res) =>{
+    res.send("we are at about")
+})
+
+app.listen(3000 ,()=>{
+    console.log("server has started")
+})
