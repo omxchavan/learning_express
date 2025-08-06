0fa936877d0cad0270c22b91c599b3bcb2303c80
omxchavan <chavanom775@gmail.com>
2025-08-06 16:05:13 +0530

    done with understanding  basic middlewares

--- project0/index.js
+++ project0/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const users = require('./test_data.json');
+const fs = require('fs');
+
 
 //created a server
 const app= express();
@@ -7,9 +9,25 @@ const app= express();
 //port listening 
 const port = 3000;
 
+
+//middleware
+app.use(express.urlencoded({extended : false}));
+
+app.use((req,res,next)=> {
+    console.log("i am middleware 1")
+    next();
+})
+
+
+ app.use( (req,res,next) =>{
+    console.log("I am middleware 2 ")
+    next()
+ })
+
+
 //get for home
 app.get("/",(req,res) => {
-    res.end("we are at home")
+    res.send("we are at home")
 })
 
 //for all users get
@@ -24,6 +42,26 @@ app.get("/users/:id",(req,res) => {
     const user = users.find(user => user.id===id);
     res.json(user);
 })
+
+
+//post route to add users
+app.post("/users", (req,res) => {
+    const data = req.body;
+    console.log(data);
+    users.push({...data, id: users.length +1});
+    fs.writeFile("./test_data" , JSON.stringify(users),(err,data) => {
+        res.json({status : "succesful" })
+    })
+
+})
+
+//route for put request
+app.put("/users",(req,res) =>{
+    const userId=Number(req.body.id);
+    console.log(userId);
+    res.json({ status : "working"})
+})
+
 
 
 app.listen(port,() => {
