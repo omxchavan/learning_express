89a737d990e833314da38c5d9b61389a425728a9
omxchavan <chavanom775@gmail.com>
2025-08-12 09:10:21 +0530

    restructured file

---
+++ basic/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("we are at home page");
+});
+
+app.get("/about", (req, res) => {
+  res.send("we are at about");
+});
+
+app.listen(3000, () => {
+  console.log("server has started");
+});
